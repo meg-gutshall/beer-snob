a198fecf90b01d7071d5c35e32c9432d4bb0aea2
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-18 19:04:48 -0400

    Start building scraper methods

--- lib/beer_snob/beers.rb
+++ lib/beer_snob/beers.rb
@@ -1,13 +1,40 @@
 require 'pry'
 
 class BeerSnob::Beers
+  attr_accessor :name, :info, :examples, :alcohol
 
-  def style_families
-    # returns all beer style families
+  def self.style_families
+
+    family_style_1 = self.new
+    family_style_1.name = "Pale Ales"
+
+    family_style_2 = self.new
+    family_style_2.name = "Dark Lagers"
+
+    family_style_3 = self.new
+    family_style_3.name = "Brown Ales"
+
+    [family_style_1, family_style_2, family_style_3]
+
   end
 
-  def beer_styles
+  def self.beer_styles
     # returns all beer styles
+    
+    beer_style_1 = self.new
+    beer_style_1.name = "American Amber Ale"
+    beer_style_1.info = "Descriptive info..."
+
+    beer_style_2 = self.new
+    beer_style_2.name = "American Amber Lager"
+    beer_style_2.info = "Descriptive info..."
+
+    beer_style_3 = self.new
+    beer_style_3.name = "American Brown Ale"
+    beer_style_3.info = "Descriptive info..."
+
+    [beer_style_1, beer_style_2, beer_style_3]
+
   end
 
 end
