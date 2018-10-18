7e88c4d08bbe6405d2e48e0ee9a2f57b9a206e45
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-18 19:04:00 -0400

    Add art for CLI

---
+++ lib/beer_snob/ASCII text art.rb
@@ -0,0 +1,18 @@
+class BeerSnob::Art
+
+  def decor
+    puts "         .:.      .:.         .:."
+    puts "       _oOoOo   _oOoOo       oOoOo_"
+    puts "      [_|||||  [_|||||       |||||_]"
+    puts "        |||||    |||||       |||||"
+    puts "  jgs   ~~~~~    ~~~~~       ~~~~~"
+  end
+
+end
+
+# original art
+#        .:.      .:.         .:.
+#      _oOoOo   _oOoOo       oOoOo_
+#     [_|||||  [_|||||       |||||_]
+#       |||||    |||||       |||||
+# jgs   ~~~~~    ~~~~~       ~~~~~
