e258fa58720a13cf10c05100a3d22e6d3cafcb92
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-19 18:23:24 -0400

    Change case statement to conditional statement
    There are too many options for a case statement to be viable in this app.
    A conditional statement works much better using indexing for the
    @family_styles variable.

--- lib/beer_snob/cli.rb
+++ lib/beer_snob/cli.rb
@@ -23,23 +23,19 @@ class BeerSnob::CLI
     input = nil
     
     while input != "exit"
-      puts "\nEnter the number of the style family you'd like to learn more about or type exit to quit."
+      puts "\nEnter the number of the beer family style you'd like to learn more about."
+      puts "Type 'list' to display the beer family styles or type 'exit' to quit."
       input = gets.strip
-      
-      case input
-      when "1"
-        puts "\nList of Pale Ale beer styles..."
-      when "2"
-        puts "\nList of Dark Lager beer styles..."
-      when "3"
-        puts "\nList of Brown Ale beer styles..."
-      # until all style families are accounted for
-      # this output will be a short description of the beer style family (?) 
-      # and a list of the beer styles under that family
-      when "list"
+
+      if input.to_i > 0
+        puts @family_styles[input.to_i - 1]
+        # this output will be a short description of the beer style family (?) 
+        # and a list of the beer styles under that family
+      elsif input == "list"
         list_family_styles
       else
-        # put else statement here
+        # put else statement here for wrong input
+        # display app instructions (a la "help")
       end
     end
 
