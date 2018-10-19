b3c90ef6180e0c8c6fc9ed88a909a31d625f93b3
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-19 19:09:41 -0400

    Add scraper.rb file to requirements

--- lib/beer_snob.rb
+++ lib/beer_snob.rb
@@ -1,8 +1,10 @@
 # Acts as the environment
 # The executable file will require this file and this file requires all 
 # other dependencies
+require "nokogiri"
+require "open-uri"
 
 require_relative "./beer_snob/version"
 require_relative "./beer_snob/beers"
 require_relative "./beer_snob/cli"
-# require_relative "./beer_snob/scraper"
+require_relative "./beer_snob/scraper"
