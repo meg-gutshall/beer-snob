a163c90f7742e1b7441a939c97cd265b45a31db7
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-21 18:12:32 -0400

    Modify cli.rb file
    Changed 'exit' to 'esc' as to avoid using a Ruby keyword as a variable.
    
    Edited #family_styles per method addition to scraper.rb file.
    
    Added #list_beer_styles and reformatted #sub_menu.

--- lib/beer_snob/cli.rb
+++ lib/beer_snob/cli.rb
@@ -7,8 +7,7 @@ class BeerSnob::CLI
     greeting
     list_family_styles
     top_menu
-    # sub_menu
-    exit
+    esc
   end
 
   def greeting
@@ -16,25 +15,26 @@ class BeerSnob::CLI
   end
 
   def list_family_styles
-    @family_styles = BeerSnob::Beers.family_styles
+    puts "\nBeer Family Styles"
+    @family_styles = BeerSnob::Scraper.family_styles
     @family_styles.each.with_index(1) do |family, i|
-      puts "#{i}. #{family.name}"
+      puts "#{i}. #{family}"
     end
   end
 
   def top_menu
     input = nil
     
-    while input != "exit"
+    while input != "esc"
       puts "\nEnter the number of the beer family style you'd like to learn more about."
-      puts "Type 'list' to display the beer family styles or type 'exit' to quit."
+      puts "Type 'list' to display the beer family styles or type 'esc'."
       input = gets.strip
 
       if input.to_i > 0
-        family = @family_styles[input.to_i - 1]
-        puts "#{family.name}"
-        # this output will be a short description of the beer style family (?) 
-        # and a list of the beer styles under that family
+        @family = @family_styles[input.to_i - 1]
+        puts "\n#{@family.chomp("s")} Beer Styles"
+        list_beer_styles
+        # this output will be a list of the beer styles under that family
       elsif input == "list"
         list_family_styles
       else
@@ -45,34 +45,43 @@ class BeerSnob::CLI
 
   end
 
+  def list_beer_styles
+    @beer_styles = BeerSnob::Scraper.new.scrape
+    @beer_styles.each.with_index(1) do |beer, i|
+      if beer[:family_style] == @family
+        puts "#{i}. #{beer[:style_name]}"
+      end
+    end
+    sub_menu
+    # TODO: fix numbering for this list
+  end
+
   # Readdress this method -- pry above method to find insertion point
   def sub_menu
+    input = nil
     
-    while input != "exit"
-      puts "\nEnter the number of the beer style you'd like to learn more about or type exit to quit."
+    while input != "esc"
+      puts "\nEnter the number of the beer style you'd like to learn more about."
+      puts "Type 'list' to display the beer styles, type 'back' to choose a different family style, or type 'esc'."
       input = gets.strip
       
-      case input
-      when "1"
-        puts "\nMore info..."
-      when "2"
-        puts "\nMore info..."
-      when "3"
-        puts "\nMore info..."
-      # until all beer styles are accounted for
-      # this output will be the scraped data of each beer style
-      when "list"
-        list_beer_styles   # will have to make this method
-      when "back"
+      if input.to_i > 0
+        family = @family_styles[input.to_i - 1]
+        puts "#{family}"
+        # this output will be a list of the beer styles under that family
+      elsif input == "list"
+        list_beer_styles
+      elsif input == "back"
         list_family_styles
         top_menu
       else
-        # put else statement here
+        # put else statement here for wrong input
+        # display app instructions (a la "help")
       end
     end
   end
 
-  def exit
+  def esc
     puts "\nLast call!"
   end
 
