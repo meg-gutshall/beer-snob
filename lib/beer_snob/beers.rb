ebb4b464684549a9b57b0bc84bb6115196aa2aab
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-21 16:35:12 -0400

    Abstract beers.rb file methods

--- lib/beer_snob/beers.rb
+++ lib/beer_snob/beers.rb
@@ -4,35 +4,13 @@ class BeerSnob::Beers
   attr_accessor :name, :info, :examples, :alcohol
 
   def self.family_styles
-    puts <<-DOC
-    \nBeer Style Families
-    1. "Pale Ales"
-    2. "Dark Lagers"
-    3. "Brown Ales"
-    4. "India Pale Ales"
-    5. "Wheat Beers"
-    6. "Strong Ales"
-    7. "Belgian Styles"
-    8. "Hybrid Beers"
-    9. "Porters"
-    10. "Stouts"
-    11. "Bocks"
-    12. "Scottish-Style Ales"
-    13. "Wild/Sour Beers"
-    14. "Piseners and Pale Lagers"
-    15. "Specialty Beers"
-  DOC
+    family_styles = []
+    family_style = BeerSnob::Scraper.new.scrape
 
-    family_style_1 = self.new
-    family_style_1.name = "Pale Ales"
 
-    family_style_2 = self.new
-    family_style_2.name = "Dark Lagers"
+    binding.pry
 
-    family_style_3 = self.new
-    family_style_3.name = "Brown Ales"
 
-    [family_style_1, family_style_2, family_style_3]
 
   end
 
