347310bbdf6d3485f4782b75fcbf3a6e5d0d81cc
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-27 18:26:43 -0400

    Add finishing touches
    Fixed terminology and spacing in cli.rb file. Added color to cli.rb
    and art.rb files.

--- lib/beer_snob/art.rb
+++ lib/beer_snob/art.rb
@@ -2,12 +2,25 @@ require "paint"
 
 class Art
 
+  # def decor
+  #   puts ""
+  #   puts "         .:.      .:.         .:."
+  #   puts "       _oOoOo   _oOoOo       oOoOo_"
+  #   puts "      [_|||||  [_|||||       |||||_]".colorize(:light_yellow)
+  #   puts "        |||||    |||||       |||||".colorize(:light_yellow)
+  #   puts "        ~~~~~    ~~~~~       ~~~~~"
+  # end
+
   def decor
     puts ""
     puts "         .:.      .:.         .:."
     puts "       _oOoOo   _oOoOo       oOoOo_"
-    puts "      [_|||||  [_|||||       |||||_]"
-    puts "        |||||    |||||       |||||"
+    print "      [_"
+    print "|||||".colorize(:light_yellow)
+    print "  [_"
+    print "|||||       |||||".colorize(:light_yellow)
+    puts "_]"
+    puts "        |||||    |||||       |||||".colorize(:light_yellow)
     puts "        ~~~~~    ~~~~~       ~~~~~"
   end
 
--- lib/beer_snob/cli.rb
+++ lib/beer_snob/cli.rb
@@ -1,6 +1,7 @@
 # CLI Controller
 require 'pry'
 
+
 class CLI
 
   def call
@@ -15,7 +16,7 @@ class CLI
   end
 
   def list_family_styles
-    puts "\nBeer Family Styles"
+    puts "\nFamily Styles".colorize(:light_magenta)
     @family_styles = []
     @beer_info.map do |beer|
       @family_styles << beer.family_name
@@ -29,30 +30,38 @@ class CLI
     input = nil
     
     while input != "exit"
-      puts "\nEnter the number of the beer family style you'd like to learn more about."
-      puts "Type 'list' to display the beer family styles or type 'exit'."
+      puts "\nEnter the number of the family style you'd like to learn more about."
+      print "\nType "
+      print "'families'".colorize(:light_green)
+      print " to display the list of family styles again or type "
+      print "'exit'".colorize(:light_green)
+      puts " to quit."
       input = gets.strip
 
-      if input.to_i > 0
+      if input.to_i > 0 && input.to_i < 16
         @family = @family_styles[input.to_i - 1]
         list_beer_styles
         sub_menu
         # this output is a list of the beer styles under that family
-      elsif input == "list"
+      elsif input.downcase == "families"
         list_family_styles
+      elsif input.downcase == "exit"
+        exit
       else
-        # put else statement here for wrong input
-        # display app instructions (a la "help")
+        puts "\nYou're flagged!".colorize(:light_red)
       end
     end
-    exit
   end
 
   def list_beer_styles
-    puts "\n#{@family.chomp("s")} Styles"
-    @beer_info.each.with_index(1) do |beer, i|
+    puts "\n#{@family}".colorize(:light_magenta)
+    @beer_styles = []
+    @count = 0
+    @beer_info.each do |beer|
       if beer.family_name == @family
-        puts "#{i}. #{beer.style_name}"
+        @beer_styles << beer
+        @count += 1
+        puts "#{@count}. #{beer.style_name}"
       end
     end
   end
@@ -60,38 +69,47 @@ class CLI
   def sub_menu
     input = nil
     
+    puts "\nEnter the number of the beer style you'd like to learn more about."
     while input != "exit"
-      puts "\nEnter the number of the beer style you'd like to learn more about."
-      puts "Type 'list' to display the beer styles, type 'back' to choose a different family style, or type 'exit'."
+      print "\nType "
+      print "'styles'".colorize(:light_green)
+      print " to display the list of beer styles again, type "
+      print "'families'".colorize(:light_green)
+      print " to display the list of family styles again or type "
+      print "'exit'".colorize(:light_green)
+      puts " to quit."
       input = gets.strip
       
-      if input.to_i > 0
-        beer = list_beer_styles[input.to_i - 1]
-        puts "\nStyle Name: #{beer.style_name}"
-        puts "Style Family: #{beer.family_name}"
-        puts "\nDescription"
+      if input.to_i > 0 && input.to_i <= @count
+        beer = @beer_styles[input.to_i - 1]
+        print "\nStyle Name:".colorize(:light_magenta)
+        puts " #{beer.style_name}"
+        print "Style Family:".colorize(:light_magenta)
+        puts " #{beer.family_name}"
+        puts "\nDescription:".colorize(:light_magenta)
         puts "#{beer.description}"
-        puts "\nU.S. Commercial Examples:"
+        puts "\nU.S. Commercial Examples:".colorize(:light_magenta)
         puts "#{beer.commercial_examples[0][:beer_name]} by #{beer.commercial_examples[0][:brewery]}"
         puts "#{beer.commercial_examples[1][:beer_name]} by #{beer.commercial_examples[1][:brewery]}"
         puts "#{beer.commercial_examples[2][:beer_name]} by #{beer.commercial_examples[2][:brewery]}"
         # this output is the beer style characteristics
-      elsif input == "list"
+      elsif input.downcase == "styles"
         list_beer_styles
-      elsif input == "back"
+        puts "\nEnter the number of the beer style you'd like to learn more about."
+      elsif input.downcase == "families"
         list_family_styles
         top_menu
+      elsif input.downcase == "exit"
+        exit
       else
-        # put else statement here for wrong input
-        # display app instructions (a la "help")
+        puts "\nYou're flagged!".colorize(:light_red)
       end
     end
-    exit
   end
 
   def exit
     Art.new.decor
-    puts "\nLast call!"
+    puts "\nLast call!".colorize(:light_green)
     puts ""
     exit!
   end
