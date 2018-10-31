2bf56bc318c79973b103c09af2136905816aa957
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-31 11:31:36 -0400

    Modify `cli.rb` file
    Made changes during the project review. I used instance variables where
    I didn’t need to.
    
    `#learn`
    Instead of assigning the scraped data
    (`Beers.create_from_scrape(Scraper.scrape)`) to an instance variable,
    I can just execute the line, allowing the `#learn` method and its
    encapsulated methods to access the data.
    
    `#list_style_families`
    The `Beers#all_family_names` helper method was employed here to clean
    up the code.
    
    `#top_menu`
    By passing `#list_beer_styles` and `#sub_menu` arguments, I’m
    eliminating the need for a variable to be created inside the method to
    determine the user’s previous input.

--- lib/beer_snob/cli.rb
+++ lib/beer_snob/cli.rb
@@ -3,7 +3,7 @@
 class CLI
 
   def learn
-    @beer_info = Beers.create_from_scrape(Scraper.scrape)
+    Beers.create_from_scrape(Scraper.scrape)
     greeting
     list_style_families
     top_menu
@@ -15,9 +15,7 @@ class CLI
 
   def list_style_families
     puts "\nBeer Style Families".colorize(:light_magenta)
-    @family_styles = []
-    @beer_info.map {|beer| @family_styles << beer.family_name}
-    @family_styles.uniq!.each.with_index(1) {|family_name, i| puts "#{i}. #{family_name}"}
+    Beers.all_family_names.each.with_index(1) {|family_name, i| puts "#{i}. #{family_name}"}
   end
 
   def top_menu
@@ -29,9 +27,8 @@ class CLI
       input = gets.strip
 
       if input.to_i > 0 && input.to_i < 16
-        @family = @family_styles[input.to_i - 1]
-        list_beer_styles
-        sub_menu
+        list_beer_styles(Beers.find_by_family_name(Beers.all_family_names[input.to_i - 1]))
+        sub_menu(Beers.find_by_family_name(Beers.all_family_names[input.to_i - 1]))
         # this output is a list of the beer styles under that family
       elsif input.downcase == "families"
         list_family_styles
@@ -43,20 +40,12 @@ class CLI
     end
   end
 
-  def list_beer_styles
-    puts "\n#{@family}".colorize(:light_magenta)
-    @beer_styles = []
-    @count = 0
-    @beer_info.each do |beer|
-      if beer.family_name == @family
-        @beer_styles << beer
-        @count += 1
-        puts "#{@count}. #{beer.style_name}"
-      end
-    end
+  def list_beer_styles(family)
+    puts "\n#{family.first.family_name}".colorize(:light_magenta)
+    family.each.with_index(1) {|beer, i| puts "#{i}. #{beer.style_name}"}
   end
 
-  def sub_menu
+  def sub_menu(beers)
     input = nil
     
     puts "\nEnter the number of the beer style you'd like to learn more about."
@@ -64,8 +53,8 @@ class CLI
       puts "\nType " + "'styles'".colorize(:light_green) + " to display the list of beer styles again, type " + "'families'".colorize(:light_green) + " to display the list of style families again or type " + "'exit'".colorize(:light_green) + " to quit."
       input = gets.strip
       
-      if input.to_i > 0 && input.to_i <= @count
-        beer = @beer_styles[input.to_i - 1]
+      if input.to_i > 0 && input.to_i <= beers.count
+        beer = beers[input.to_i - 1]
         puts "\nStyle Name:".colorize(:light_magenta) + " #{beer.style_name}"
         puts "Style Family:".colorize(:light_magenta) + " #{beer.family_name}"
         puts "\nDescription:".colorize(:light_magenta)
